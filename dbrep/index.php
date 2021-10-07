f84316d40314722d5d499b845bb9f739dac10a3e
sawasick <50393229+sawasick@users.noreply.github.com>
2021-10-07 23:49:06 +0900

    Search Replace DBの入力値の入れ替え機能実装

--- dbrep/index.php
+++ dbrep/index.php
@@ -5466,6 +5466,8 @@ class icit_srdb_ui extends icit_srdb {
 
             })(jQuery);
         </script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="script.js"></script>t></script>
         </body>
         </html>
         <?php
